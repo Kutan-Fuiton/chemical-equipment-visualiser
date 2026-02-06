db0116c8343ebdef4c842a9ca3ef1d9312cf65f4
Kutan-Fuiton <subarnochakraborty123@gmail.com>
2026-02-06 19:07:35 +0530

    deployment ready

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from corsheaders.defaults import default_headers
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d@*94po(fy$*g+y$s4_b%+oy0^l)5#7hv=(@u4x0xfse3s#1mj'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-d@*94po(fy$*g+y$s4_b%+oy0^l)5#7hv=(@u4x0xfse3s#1mj')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
 
 # Application definition
